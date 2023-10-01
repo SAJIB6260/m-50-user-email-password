102c4fe2a3b6af761ed31c8982e3ed3b60ca1632
SAJIB6260 <s.k.sajib6260@gmail.com>
2023-10-02 01:05:16 +0600

    normal register form and hero register form done

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,27 @@
+
+const Register = () => {
+
+    const handleRegister = e =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password)
+    }
+
+    return (
+        <div>
+            <div className="mx-auto w-1/4">
+            <h2 className="text-3xl mb-8">Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <span className="text-2xl">Email: </span><input className="mb-4 border bg-base-300 w-full px-4 py-2" type="email" name="email" placeholder="Email Address" id="" />
+                <br />
+                <span className="text-2xl">Password: </span><input className="mb-4 border bg-base-300 w-full px-4 py-2" type="password" name="password" placeholder="Password" id="" />
+                <br />
+                <input className="btn btn-secondary w-full px-4 py-2" type="submit" value="Register" />
+            </form>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
