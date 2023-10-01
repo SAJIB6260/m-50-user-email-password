f245315d244bdeb46d20da0c865587b9357ffddd
SAJIB6260 <s.k.sajib6260@gmail.com>
2023-10-02 02:36:29 +0600

    error validation in different way

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,37 +1,63 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
+import { useState } from "react";
 
 
 const Register = () => {
 
-    const handleRegister = e =>{
+    const [registerError, setRegisterError] = useState('');
+    const [success, setSuccess] = useState('')
+
+    const handleRegister = e => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password)
 
+
+        // error reset
+        setRegisterError('');
+        setSuccess('');
+
+
+        if (password.length < 6) {
+            setRegisterError('Password should be at least 6 characters or longer');
+            return;
+        }
+
         // create user
 
         createUserWithEmailAndPassword(auth, email, password)
-        .then(result =>{
-            console.log(result.user)
-        })
-        .catch(error =>{
-            console.error(error)
-        })
+            .then(result => {
+                console.log(result.user)
+                setSuccess('user created successfully')
+            })
+            .catch(error => {
+                console.log(error)
+                setRegisterError(error.message)
+            })
     }
 
     return (
         <div>
             <div className="mx-auto w-1/4">
-            <h2 className="text-3xl mb-8">Please Register</h2>
-            <form onSubmit={handleRegister}>
-                <span className="text-2xl">Email: </span><input className="mb-4 border bg-base-300 w-full px-4 py-2" type="email" name="email" placeholder="Email Address" id="" />
-                <br />
-                <span className="text-2xl">Password: </span><input className="mb-4 border bg-base-300 w-full px-4 py-2" type="password" name="password" placeholder="Password" id="" />
-                <br />
-                <input className="btn btn-secondary w-full px-4 py-2" type="submit" value="Register" />
-            </form>
+                <h2 className="text-3xl mb-8">Please Register</h2>
+                <form onSubmit={handleRegister}>
+                    <span className="text-2xl">Email: </span><input className="mb-4 border bg-base-300 w-full px-4 py-2" type="email" name="email" placeholder="Email Address" id="" required />
+                    {/* {
+                        registerError && <p className="text-red-600">{registerError}</p>
+                    } */}
+                    <br />
+                    <span className="text-2xl">Password: </span><input className="mb-4 border bg-base-300 w-full px-4 py-2" type="password" name="password" placeholder="Password" id="" required />
+                    <br />
+                    <input className="btn btn-secondary w-full px-4 py-2" type="submit" value="Register" />
+                </form>
+                {
+                    registerError && <p className="text-red-600">{registerError}</p>
+                }
+                {
+                    success && <p className="text-green-700">{success}</p>
+                }
             </div>
         </div>
     );
