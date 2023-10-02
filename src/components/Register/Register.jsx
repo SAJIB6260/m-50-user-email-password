5959b140efe4cd2501f970ccaac4d47d1d0dee69
SAJIB6260 <s.k.sajib6260@gmail.com>
2023-10-02 15:48:24 +0600

    password validation with regular expression and show password with icon

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,12 +1,14 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 
 const Register = () => {
 
     const [registerError, setRegisterError] = useState('');
-    const [success, setSuccess] = useState('')
+    const [success, setSuccess] = useState('');
+    const [showPassword, setShowPassword] = useState(false)
 
     const handleRegister = e => {
         e.preventDefault();
@@ -20,8 +22,12 @@ const Register = () => {
         setSuccess('');
 
 
-        if (password.length < 6) {
+        if (password.length < 8) {
             setRegisterError('Password should be at least 6 characters or longer');
+            return;
+        }
+        else if(!/[A-Z]/.test(password)){
+            setRegisterError('your password at least one UpperCase characters');
             return;
         }
 
@@ -40,17 +46,29 @@ const Register = () => {
 
     return (
         <div>
-            <div className="mx-auto w-1/4">
+            <div className="mx-auto w-1/2">
                 <h2 className="text-3xl mb-8">Please Register</h2>
                 <form onSubmit={handleRegister}>
-                    <span className="text-2xl">Email: </span><input className="mb-4 border bg-base-300 w-full px-4 py-2" type="email" name="email" placeholder="Email Address" id="" required />
+                    <span className="text-2xl">Email: </span><br />
+                    <input className="mb-4 border bg-base-300 w-3/4 px-4 py-2" type="email" name="email" placeholder="Email Address" id="" required />
                     {/* {
                         registerError && <p className="text-red-600">{registerError}</p>
                     } */}
                     <br />
-                    <span className="text-2xl">Password: </span><input className="mb-4 border bg-base-300 w-full px-4 py-2" type="password" name="password" placeholder="Password" id="" required />
+                    <span className="text-2xl">Password: </span><br />
+                    <input className="mb-4 border bg-base-300 w-3/4 px-4 py-2" 
+                    type={showPassword ? "text" : "password"} 
+                    name="password" 
+                    placeholder="Password" 
+                    id="" 
+                    required />
+                    <span onClick={() => setShowPassword(!showPassword)}>
+                        {
+                            showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                        }
+                    </span>
                     <br />
-                    <input className="btn btn-secondary w-full px-4 py-2" type="submit" value="Register" />
+                    <input className="btn btn-secondary w-3/4 px-4 py-2" type="submit" value="Register" />
                 </form>
                 {
                     registerError && <p className="text-red-600">{registerError}</p>
