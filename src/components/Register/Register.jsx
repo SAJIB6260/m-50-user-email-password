04653936aaf6c089508d692a211423f028d42cd5
SAJIB6260 <s.k.sajib6260@gmail.com>
2023-10-02 20:26:14 +0600

    Login users,accept terms,forgate or reset passsword done

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,6 +2,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
@@ -14,6 +15,7 @@ const Register = () => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
+        const accepted = e.target.terms.checked;
         console.log(email, password)
 
 
@@ -28,6 +30,10 @@ const Register = () => {
         }
         else if(!/[A-Z]/.test(password)){
             setRegisterError('your password at least one UpperCase characters');
+            return;
+        }
+        else if(!accepted){
+            setRegisterError('Please accept our terms and conditions')
             return;
         }
 
@@ -50,25 +56,32 @@ const Register = () => {
                 <h2 className="text-3xl mb-8">Please Register</h2>
                 <form onSubmit={handleRegister}>
                     <span className="text-2xl">Email: </span><br />
-                    <input className="mb-4 border bg-base-300 w-3/4 px-4 py-2" type="email" name="email" placeholder="Email Address" id="" required />
+                    <input className="mb-4 border bg-base-300 w-full px-4 py-2" type="email" name="email" placeholder="Email Address" id="" required />
                     {/* {
                         registerError && <p className="text-red-600">{registerError}</p>
                     } */}
                     <br />
                     <span className="text-2xl">Password: </span><br />
-                    <input className="mb-4 border bg-base-300 w-3/4 px-4 py-2" 
+                    <div className="mb-4 relative">
+                    <input className="border bg-base-300 w-full px-4 py-2" 
                     type={showPassword ? "text" : "password"} 
                     name="password" 
                     placeholder="Password" 
                     id="" 
                     required />
-                    <span onClick={() => setShowPassword(!showPassword)}>
+                    <span className="absolute top-4 right-2" onClick={() => setShowPassword(!showPassword)}>
                         {
                             showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
                         }
                     </span>
+                    </div>
                     <br />
-                    <input className="btn btn-secondary w-3/4 px-4 py-2" type="submit" value="Register" />
+                    <div>
+                        <input type="checkbox" name="terms" id="terms" />
+                        <label className="ml-2" htmlFor="terms">Accept our <a href="">Terms and Conditions</a></label>
+                    </div>
+                    <br />
+                    <input className="btn btn-secondary w-full px-4 py-2" type="submit" value="Register" />
                 </form>
                 {
                     registerError && <p className="text-red-600">{registerError}</p>
@@ -76,6 +89,7 @@ const Register = () => {
                 {
                     success && <p className="text-green-700">{success}</p>
                 }
+                <p>Already have an account? Please  <Link to="/login">Login</Link></p>
             </div>
         </div>
     );
